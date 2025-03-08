1d6d1a638204f6d10cfd1f781431b5e1bb232ce2
sanjaygit1 <152388009+sanjaygit1@users.noreply.github.com>
2025-03-08 15:19:09 +0530

    Update Stable_ElememtRecp.java

--- AdvanceSelenium_QCO-SOEAJD-A9/src/test/java/Locators/Stable_ElememtRecp.java
+++ AdvanceSelenium_QCO-SOEAJD-A9/src/test/java/Locators/Stable_ElememtRecp.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Stable_ElememtRecp {
 //im pulling
 	//im pushing the code
+
+	//im pulling back
 	public static void main(String[] args) {
 		
 		WebDriver driver=new ChromeDriver();
