7170847be82c167395ae677739ea266665c460e2
sanjaygit1 <rathinreddy1402@gmail.com>
2025-03-08 15:17:53 +0530

    push2

--- AdvanceSelenium_QCO-SOEAJD-A9/src/test/java/Locators/Stable_ElememtRecp.java
+++ AdvanceSelenium_QCO-SOEAJD-A9/src/test/java/Locators/Stable_ElememtRecp.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Stable_ElememtRecp {
 //im pulling
+	//im pushing the code
 	public static void main(String[] args) {
 		
 		WebDriver driver=new ChromeDriver();
